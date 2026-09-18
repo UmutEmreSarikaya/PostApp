f72bc248202dc463942d9ef8b9b72d98d307dc6a
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2026-09-18 21:29:46 +0300

    Fix Android convention plugin

--- build-logic/convention/src/main/kotlin/com/uesar/postapp/convention/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/uesar/postapp/convention/AndroidLibraryConventionPlugin.kt
@@ -12,7 +12,6 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("com.android.library")
-            pluginManager.apply("org.jetbrains.kotlin.android")
 
             extensions.configure<LibraryExtension> {
                 compileSdk = 37
